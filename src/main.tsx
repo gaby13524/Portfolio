225bd94036fb006c5302033201156905b280afe4
gbongz <54719206+gaby13524@users.noreply.github.com>
2026-02-21 20:50:36 +0100

    setup

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,21 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import { Intro } from "./components/Intro";
 
-createRoot(document.getElementById('root')!).render(
+function App() {
+  return (
+    <div className="App">
+      <Intro />
+      <div className="text-center text-2xl mt-4">
+        Page is still under construction...
+      </div>
+    </div>
+  );
+}
+
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <App />
   </StrictMode>,
-)
+);
