51b946c8e67ca175cf53e5de9bd60b69bff5e130
gbongz <54719206+gaby13524@users.noreply.github.com>
2026-02-23 12:09:12 +0100

    add react-type-animation and update Intro component styling

--- src/components/Intro.tsx
+++ src/components/Intro.tsx
@@ -1,56 +1,44 @@
-import { useState } from "react";
+// import { useState } from "react";
+import { Button } from "./ui/button";
+import { TypeAnimation } from "react-type-animation";
 
 export function Intro() {
-  const [scroll, setScroll] = useState<Boolean>(false);
-  const [u_sure, setU_sure] = useState<Boolean>(false);
   //    bg-indigo-700 text-white;
   return (
-    <div className="h-screen flex items-center justify-center gap-4">
-      <div className="mb-4 items-center justify-between w-full md:w-1/2 px-4">
-        <h5>Hi there, I am</h5>
-        <h1 className="text-center">Gabriela Djuhadi.</h1>
-        <p className="text-md">
-          This portfolio is built because
-          <ul className="list-disc list-inside">
-            <li>I need a job</li>
-            <li>
-              I am still heartbroken and would like to archive memories from my
-              prev job.
-            </li>
-          </ul>
+    <div className="h-screen flex items-center bg-indigo-900 text-white">
+      <div className="mb-4 w-full lg:w-1/2 px-10 gap-4 font-outfit">
+        <h5 className="text-2xl">Hi there, my name is</h5>
+        <h1 className="text-center m-4">Gabriela Djuhadi.</h1>
+        <p className="text-lg/8">
+          I am a Fullstack Developer trying to find myself and my niche in a sea
+          of others. I am one of a kind, for better or worse; get to know me if
+          you dare (I need a job).
         </p>
+        <Button variant="rose" className="mx-auto block" onClick={() => {}}>
+          Hire me!
+        </Button>
       </div>
-      <div className="mb-4 flex flex-col text-end w-full md:w-1/2">
-        <p className="font-bitcount text-4xl">
-          Bioinformatician by training, web developer by (former) part-time job.
-          Full-time cat-servant and former <s>personality hire</s> event
-          organizer.
-        </p>
-        <button onClick={() => setU_sure(true)}>Hire me!</button>
-        {u_sure && (
-          <div>
-            <p>
-              While skipping the resume? I recommend at least skimming through
-              it first.
-            </p>
-            <button>
-              I'm good, let's see your rates.
-              {/* TODO: skip to the rates and add a note that i'll still allow them to scroll */}
-            </button>
-            <button
-              onClick={() => {
-                setU_sure(false);
-                setScroll(true);
-              }}
-            >
-              Sure, let's do that.
-            </button>
-          </div>
-        )}
-        {scroll && <p>Gate is open, scroll down</p>}
+      <div className="mb-4 text-end text-balance w-full md:w-1/2 px-10">
+        <TypeAnimation
+          sequence={[
+            "Bioinformatician by training.",
+            500, // pause 1.5s
+            "Bioinformatician by training. Web developer by (former) part-time job.",
+            500, // pause 0.5s
+            "Bioinformatician by training. Web developer by (former) part-time job. Full-time cat-servant and former personality hire",
+            1500,
+            "Bioinformatician by training. Web developer by (former) part-time job. Full-time cat-servant and former ",
+            1500,
+            "Bioinformatician by training. Web developer by (former) part-time job. Full-time cat-servant and former event organizer.",
+          ]}
+          deletionSpeed={10}
+          speed={10}
+          repeat={0}
+          className="font-bitcount text-4xl"
+        />
       </div>
     </div>
   );
 }
-
+// I am still heartbroken and would like to archive memories from my prev job.
 // please only contact me through email
