c6228dcd845a6b55ff6aa5ceab6c175e002bc574
gbongz <54719206+gaby13524@users.noreply.github.com>
2026-03-24 21:32:37 +0100

    just some addition to the construction msg

--- src/main.tsx
+++ src/main.tsx
@@ -9,11 +9,10 @@ function App() {
     <div className="App">
       <Intro />
       <Experience />
-      {/* <div className="text-center text-2xl m-4">
-        Page is still under construction... You can email me for inquiries or
-        just to say hi! (I need a job)
-        <p>gabglodj@gmail.com</p>
-      </div> */}
+      <div className="text-center text-2xl w-3/4 mx-auto mb-8 bg-burnt_peach-500 rounded-lg p-6">
+        Page is still under construction ... You can email me for inquiries or
+        just to say hi! (<strong>I need a job</strong>)<p>gabglodj@gmail.com</p>
+      </div>
     </div>
   );
 }
